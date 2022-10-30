36aafc1db54d1c3f8b8f458463785b29c8bd2800
Ngoufoemmerick <ngoufoemmerick@yahoo.fr>
2022-10-30 13:15:11 +0000

    monolithique to microservice

---
+++ env.bat
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=admin1234
+set POSTGRES_HOST=udagram-instance.cahedyejsspr.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=udagram_db
+set AWS_BUCKET=udagram-bucket-instagram
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
